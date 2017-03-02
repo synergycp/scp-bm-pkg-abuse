921eaf1ed6a582c7445996eb1ebaf613f1b2a508
php1 <api@itsvit.org>
2017-03-02 16:35:32 +0200

    reverted .idea

--- client/app/report/list/list.index.controller.js
+++ client/app/report/list/list.index.controller.js
@@ -26,11 +26,11 @@
     vm.tabs = {
       active: parseInt($stateParams.tab) || 0,
       items: [
+        new Tab(LANG+'.tab.unanswered', {
+            pending_client: true,
+        }),
         new Tab(LANG+'.tab.answered', {
           pending_admin: true,
-        }),
-        new Tab(LANG+'.tab.unanswered', {
-          pending_client: true,
         }),
         new Tab(LANG+'.tab.archive', {
           archive: true,
