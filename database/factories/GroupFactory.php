0113030bd075d7b258e5cf5844e6f78dc454bedc
php1 <api@itsvit.org>
2017-03-03 16:42:17 +0200

    remove factories

--- database/factories/GroupFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Group;
-
-$factory->define(Group\Group::class, function (Faker\Generator $faker) {
-    return [
-        'name' => $faker->name,
-    ];
-});
--- database/factories/ServerPortFactory.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$factory->define(App\Server\Port\Port::class, function (Faker\Generator $faker) {
-    return [
-        'mac' => $faker->macAddress,
-    ];
-});
