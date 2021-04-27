6ab15e12f7c0a9b74cfe757577462bd6c6dbe91d
Zane Hooper <zanehoop@gmail.com>
2021-04-27 09:20:53 -0400

    Version manifest generation

--- admin/gulpfile.js
+++ admin/gulpfile.js
@@ -1,44 +1,56 @@
-var gulp = require('scp-ng-gulp')(require('gulp'));
+var gulp = require("scp-ng-gulp")(require("gulp"));
 
-gulp.require('settings').dir = __dirname;
+gulp.require("settings").dir = __dirname;
 
 var PATH = {
-  PUBLIC: '../public/admin/',
-  MARKUP: 'app/',
-  SCRIPTS: 'app/',
-  ASSETS: 'resources/assets/',
+  PUBLIC: "../public/admin/",
+  MARKUP: "app/",
+  SCRIPTS: "app/",
+  ASSETS: "resources/assets/"
 };
 var js = {
   src: PATH.SCRIPTS,
-  app: 'app.js',
+  app: "app.js"
 };
 
-var scripts = gulp.require('scripts');
-gulp.task('scripts', scripts.app({
-  dest: PATH.PUBLIC + js.app,
-  src: [
-    PATH.SCRIPTS + '**/*.module.js',
-    PATH.SCRIPTS + '**/*.js'
-  ],
-}));
+var versionManifest = gulp.require("version-manifest")({
+  build: PATH.PUBLIC,
+  files: [PATH.PUBLIC + "**/*.*"],
+  package: "abuse/admin"
+});
 
-var templates = gulp.require('templates');
-gulp.task('templates', templates({
-  src: [PATH.MARKUP + '**/*.pug'],
-  dest: PATH.PUBLIC,
-}));
+var scripts = gulp.require("scripts");
+gulp.task(
+  "scripts",
+  scripts.app({
+    dest: PATH.PUBLIC + js.app,
+    src: [PATH.SCRIPTS + "**/*.module.js", PATH.SCRIPTS + "**/*.js"]
+  })
+);
 
-var copy = gulp.require('copy');
-gulp.task('copy', copy({
-  src: PATH.ASSETS+'**/*.*',
-  dest: PATH.PUBLIC,
-  base: 'resources',
-}));
+var templates = gulp.require("templates");
+gulp.task(
+  "templates",
+  templates({
+    src: [PATH.MARKUP + "**/*.pug"],
+    dest: PATH.PUBLIC
+  })
+);
 
+var copy = gulp.require("copy");
+gulp.task(
+  "copy",
+  copy({
+    src: PATH.ASSETS + "**/*.*",
+    dest: PATH.PUBLIC,
+    base: "resources"
+  })
+);
 
-gulp.task('default', gulp.parallel([
-  'copy',
-  'templates',
-  'scripts',
-]));
-gulp.task('build', gulp.series(['default']));
+gulp.task("manifest", versionManifest);
+
+gulp.task(
+  "default",
+  gulp.series([gulp.parallel(["copy", "templates", "scripts"]), "manifest"])
+);
+gulp.task("build", gulp.series(["default"]));
