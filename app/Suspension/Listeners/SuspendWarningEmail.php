47b86ac1330fa11906868ff0c3344c0d918c7c83
Damian Harouff <damian@synergycp.com>
2026-03-18 18:05:02 -0700

    Fix negative days in suspension warning email and bump version to 2.1.1
    Carbon 3 (Laravel 11) changed diffInDays() to return signed floats.
    Added abs() to fix negative values and round() to cap at 2 decimal places.

--- app/Suspension/Listeners/SuspendWarningEmail.php
+++ app/Suspension/Listeners/SuspendWarningEmail.php
@@ -64,7 +64,7 @@ extends Mail\EmailListener
     protected function send(Server $server, Carbon $createdDate)
     {
         $date = $this->suspension->maxReportDate();
-        $days = $createdDate->diffInDays($date) + 1;
+        $days = round(abs($createdDate->diffInDays($date)) + 1, 2);
         $context = [
             'server' => $server->expose('name'),
             'report' => [
