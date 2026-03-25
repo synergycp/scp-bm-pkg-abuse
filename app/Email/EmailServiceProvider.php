8846e8cbaab82fbfc3f9cd2fdef4e95686c1d3ee
damianharouff <2953243+damianharouff@users.noreply.github.com>
2026-03-25 16:22:03 -0700

    Revert Setting::saving listener that crashed the app
    The model listener on EmailServiceProvider boot caused a fatal error.
    Reverted to original EmailServiceProvider. Bump version to 2.2.8.

--- app/Email/EmailServiceProvider.php
+++ app/Email/EmailServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Packages\Abuse\App\Email;
 
-use App\Setting\Setting;
 use Illuminate\Console\Scheduling\Schedule;
 use App\Support\ScheduleServiceProvider as ServiceProvider;
 
@@ -18,19 +17,6 @@ extends ServiceProvider
     protected $commands = [
         Commands\SyncAbuseEmailsCommand::class,
     ];
-
-    public function boot()
-    {
-        parent::boot();
-
-        // Prevent null values on the archive_folder setting (Laravel's
-        // ConvertEmptyStringsToNull middleware converts '' to null).
-        Setting::saving(function (Setting $setting) {
-            if ($setting->name === 'pkg.abuse.auth.archive_folder' && is_null($setting->value)) {
-                $setting->value = '';
-            }
-        });
-    }
 
     protected function schedule(Schedule $schedule)
     {
